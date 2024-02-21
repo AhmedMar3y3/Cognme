2a4f62046b6716bc8b61fb562881a4b542cadc80
ahmedmar3y1 <86689481+ahmedmar3y1@users.noreply.github.com>
2024-02-21 20:07:13 +0200

    first commit

---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,20 @@
+<?php 
+namespace App\Traits ;
+trait HttpResponses{
+    protected function success($data, $message = null , $code = 200){
+        return response()
+        ->json([
+            'status' => 'request was successful',
+            'data' => $data,
+            'message' => $message 
+        ], $code); 
+    }
+    protected function error($data, $message = null , $code){
+        return response()
+        ->json([
+            'status' => 'an error has occured',
+            'data' => $data,
+            'message' => $message 
+        ], $code); 
+    }
+ }
